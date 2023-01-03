c7ff9243319a18bf5a0e9c2c64b93583948902b4
Marek Bernat <mbernat@users.noreply.github.com>
2023-01-03 19:30:51 +0100

    Fully fix rendering shaders

--- src/gpu.rs
+++ src/gpu.rs
@@ -198,30 +198,34 @@ impl RenderState {
 var<storage, read_write> t: array<f32>;
 
 struct Info {
+    // Values in -1..1 in the vertex shader but in 0..screen_res in the fragment shader
     @builtin(position) pos: vec4<f32>,
-    @location(0) color: vec4<f32>,
+    @location(0) fraction: vec4<f32>,
 }
 
 @vertex
 fn vertex(@builtin(vertex_index) v: u32, @builtin(instance_index) i: u32) -> Info {
+    var pos: vec2<f32>;
     if i == 0u {
         switch v {
-            case 0u: { let v = vec4(0.0, 0.0, 0.0, 1.0); return Info(v, v); }
-            case 1u: { let v = vec4(1.0, 0.0, 0.0, 1.0); return Info(v, v); }
-            default: { let v = vec4(0.0, 1.0, 0.0, 1.0); return Info(v, v); }
+            case 0u: { pos = vec2(-1.0, -1.0); }
+            case 1u: { pos = vec2(1.0, -1.0); }
+            default: { pos = vec2(-1.0, 1.0); }
         }
     } else {
         switch v {
-            case 0u: { let v = vec4(1.0, 0.0, 0.0, 1.0); return Info(v, v); }
-            case 1u: { let v = vec4(1.0, 1.0, 0.0, 1.0); return Info(v, v); }
-            default: { let v = vec4(0.0, 1.0, 0.0, 1.0); return Info(v, v); }
+            case 0u: { pos = vec2(1.0, -1.0); }
+            case 1u: { pos = vec2(1.0, 1.0); }
+            default: { pos = vec2(-1.0, 1.0); }
         }
     }
+    let uni = (pos + 1.0) / 2.0;
+    return Info(vec4(pos, 0.0, 1.0), vec4(uni, 0.5, 1.0));
 }
 
 @fragment
 fn fragment(info: Info) -> @location(0) vec4<f32> {
-    return info.color;
+    return info.fraction;
 }
 "
             .into(),
